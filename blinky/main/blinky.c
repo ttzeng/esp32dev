7780c1bf0f95b57a4be0d314f8840c042dbfbcbb
Tonny Tzeng <tonny.tzeng@gmail.com>
2025-05-30 18:01:07 +0800

    blinky: output messages through the UART as well Signed-off-by: <tonny.tzeng@gmail.com>

--- blinky/main/blinky.c
+++ blinky/main/blinky.c
@@ -1,8 +1,12 @@
+#include <string.h>
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/uart.h"
 #include "sdkconfig.h"
+
+#define RX_BUF_SIZE 1024
 
 void task_blinky(void *pvParameter)
 {
@@ -13,8 +17,12 @@ void task_blinky(void *pvParameter)
 
     int state = 0;
     while (1) {
+        char str[10];
         gpio_set_level(CONFIG_GPIO_LED, state ^= 1);
-        printf("LED %s\n", state? "Off" : "On");
+        snprintf(str, sizeof(str), "LED %s\r\n", state ? "Off" : "On");
+        printf("%s", str);
+
+        uart_write_bytes(UART_NUM_0, str, strlen(str));
 
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
@@ -22,5 +30,17 @@ void task_blinky(void *pvParameter)
 
 void app_main(void)
 {
+    // Initialize the UART for output
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    };
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, RX_BUF_SIZE, 0, 0, NULL, 0));
+
     xTaskCreate(task_blinky, "task_blinky", 4096, NULL, 5, NULL);
 }
